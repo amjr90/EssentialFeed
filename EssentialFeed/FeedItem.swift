90e02544985dbe1e346ac5f4e601f9657007e558
andres jaramillo <andres.jaramillo@celuweb.com>
2023-01-10 16:20:21 -0500

    initial commit

---
+++ EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by andres jaramillo on 10/01/23.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imateURL: URL
+}
